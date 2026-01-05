de90ed7e2552b667c261c544f718eb856861bdbb
idunno-bruh <andrea8adrenalyn@gmail.com>
2026-01-05 19:31:04 +0100

    added model and executing routine, missing training

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import  {Neuron, sigmoid, Weigth, generateNeurons, generateWeigths} from "./lib/main.js"
+import  {Neuron, sigmoid, Weigth, generateNeurons, generateWeigths, Layer, Model} from "./lib/main.js"
 let n = 0
 function indexing() {
     n++
     return n
 }
-let list = generateNeurons(4, 1, 0, false, indexing)
-let test = new Neuron(0, 0, indexing())
-let weigths = generateWeigths(list, [test], [1 / 4], false)
-test.calculateValue(weigths, list)
-test.verbose()
+let lists = [generateNeurons(2, 0, 0, true, indexing), generateNeurons(16, 0, 0, true, indexing), generateNeurons(16, 0, 0, true, indexing), generateNeurons(1, 0, 0, true, indexing)]
+let weights = [generateWeigths(lists[0], lists[1], 0, true), generateWeigths(lists[1], lists[2], 0, true), generateWeigths(lists[2], lists[3], 0, true)]
+let starting = new Layer(lists[0], [], weights[0])
+let first = new Layer(lists[1], weights[0], weights[1])
+let second = new Layer(lists[2], weights[1], weights[2])
+let last = new Layer(lists[3], weights[2], [])
+let model = new Model([starting, first, second, last])
+let list = model.execute([1, 3])
+list[0].verbose()
