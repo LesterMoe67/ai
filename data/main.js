f674b81324a14358884e918b7b98181d1ef0992e
idunno-bruh <andrea8adrenalyn@gmail.com>
2026-05-17 22:01:03 +0200

    trying to implement softmax

--- data/main.js
+++ data/main.js
@@ -1,57 +1,11 @@
-import * as fs from "fs"
-export function readFile() {
-    let obj = JSON.parse(fs.readFileSync("./data/main.json", "utf8"))
-    return obj
-}
-export function write(content) {
-    fs.writeFileSync("./data/main.json", content, "utf-8") 
-}
-export function push(content) {
-    let obj = readFile()
-    obj.push(content)
-    write(JSON.stringify(obj))
-} 
-export function clear() {
-    write("[]")
-
+import * as fs from "fs" 
+import PromptSync from "prompt-sync"
+ export function saveModel(obj, name) {
+    fs.writeFileSync("./model/" + name + ".json", JSON.stringify(obj), "utf8")
  }
-export function writeGradients(model, i) {
-    let layers = []
-    for (let i = 0; i < model.layers.length; i++) {
-        let listDelta = []
-        let listGradient = []
-        for (let vector of model.layers[i].delta.vectors) {
-            let temp = []
-            for (let element of vector.list) {
-                temp.push(element)
-            }
-            listDelta.push(temp)
-        }
-        if (i != 0) {
-            
-            for (let vector of model.layers[i].gradients.vectors) {
-                let temp = []
-                for (let element of vector.list) {
-                    temp.push(element)
-                }
-                listGradient.push(temp)
-            }
-        }
-        let obj = {"layer" : i, "deltas" : listDelta, "gradients" : listGradient}
-        layers.push(obj)
-    }
-    let object = {"model" : layers, "index" : i}
-    push(object)
-}
-export function readGradients(i) {
-    let list = readFile()
-    for (let obj of list) {
-        if (obj.index == i) {
-            let layers = []
-            for (let layer of obj.model) {
-                layers.push([layer.deltas, layer.gradients])
-            }
-            return layers
-        }
-    }
-}
+ export function getModel(name) {
+    return JSON.parse(fs.readFileSync("./model/"+ name + ".json", "utf8"))
+ }
+ export function input(string) {
+    return PromptSync({autocomplete : [fs.readdirSync("./model").forEach((value) => {value.slice(0, value.length-5)})]})(string)
+ }
--- server.js
+++ server.js
@@ -1,10 +1,64 @@
-import  {Neuron, sigmoid, Weigth, Layer, Model, sameSet, Vector, Matrix, xavier, generateModel, matrixMultiplication, matrixAddition, vectorElementMultiplication, weigthMatrixes, vectorAddition, readGradientsMatrixes, averageMatrix, generateInputs} from "./lib/main.js"
-import { clear, push, readFile,  readGradients,  writeGradients} from "./data/main.js"
-let a = new Matrix(2, [new Vector(2,[5,8]),new Vector(2,[9,2])])
-let b = new Matrix(2, [new Vector(2, [4, 1]), new Vector(2, [3, 2])])
-let vecA = new Vector(2, [2, 4])
-let vecB = new Vector(2, [2, 2,])
-let model = generateModel(4, [4, 16, 16, 1])
-let data = generateInputs(4, 800)
+import { input } from "./data/main.js"
+import  {Neuron, sigmoid, Weigth, Layer, Model, sameSet, Vector, Matrix, xavier, generateModel, matrixMultiplication, matrixAddition, vectorElementMultiplication, weigthMatrixes, vectorAddition, averageMatrix, generateInputs, loadModel} from "./lib/main.js"
+let exit = false
+    let model;
+function training(list) {
+    let output = []
+    for (let number of list) {
+        if (number == Math.max(...list)) {
+            output.push(1)
+        }
+        else {
+            output.push(0)
+        }
+    }
+    return output
+}
+while (!exit) {
+    let output = input("comand : ")
 
-model.train(10, 8, data[0], data[1], 0.01)
+    switch(output) {
+        case "exit" :
+            exit = true
+            break;
+        case "load" :
+            let name = input("name : ")
+            model = loadModel(name)
+            break;
+        case "create" :
+            let layers = input("number of layers : ")
+            let list = []
+            for (let i = 0; i < Number.parseInt(layers); i++) {
+                list.push(Number.parseInt(input("neurons on layer number " + (i + 1)+  " : ")))
+            }
+            let softmax = input("softmax : ") == "true"
+            model = generateModel(Number.parseInt(layers), list, softmax)
+            break;
+        case "train":
+            if (model == undefined) {
+                console.log("no model available ")
+            } else {
+                let epochs = Number.parseInt(input("epochs : "))
+                let inputSize = Number.parseInt(input("dataset size : "))
+                let batchsize = Number.parseInt(input("batch size : "))
+                let learingRate = Number.parseInt(input("learing rate : "))
+                let data = generateInputs(model.layers[0].neurons.length, inputSize, training)
+                model.train(epochs, batchsize, data[0], data[1], learingRate)
+            }
+            break;
+        case "save":
+            let nae = input("name : ")
+            model.save(nae)
+            break;
+        case "predict":
+            let inputs = []
+            for (let i = 0; i < model.layers[0].neurons.length;i++) {
+                inputs.push(Number.parseInt(input("input number " + (i + 1) + " : ")))
+            }
+            model.forward(inputs)
+            console.log(model.layers[model.layers.length - 1].neurons)
+            break;
+        default:
+            console.log("sybau")
+    }
+}
