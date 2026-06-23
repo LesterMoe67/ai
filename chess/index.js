66a2eb27b1b6554a1962c469055739cdde553692
idunno-bruh <andrea8adrenalyn@gmail.com>
2026-06-23 11:38:43 +0200

    worked on move gen and added takes moves and turns

--- chess/index.js
+++ chess/index.js
@@ -2,6 +2,7 @@ import { fenToBoard } from "./lib/bot.js"
 import { Piece } from "./lib/classes.js"
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+let turn = "white"
 let xOffset = 1920/2 - 80*8/4*3
 let yOffset = 20
 let blackLoaded = false
@@ -23,6 +24,7 @@ white.onload = () => {
 }
 white.src = "./assets/white-tile.png"
 function draw() {
+    console.log("cleared")
     ctx.clearRect(0, 0, 1920, 800)
     for (let a = 0; a < 8; a++) {
         for (let b = 0; b < 8; b++) {
@@ -47,7 +49,6 @@ let pieces = fenToBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 for (let peice of pieces) {
     drawPiece(peice)
 }
-
 function highlight(coords) {
     let occupied = undefined
     for (let piece of pieces) {
@@ -75,7 +76,25 @@ function highlight(coords) {
 
     }
 }
-
+function handleMove(pieces, coords, moving) {
+    let newList = []
+    for (let piece of pieces) {
+        if (!(piece.coordinates[0] == coords[0] && piece.coordinates[1] == coords[1]) ) {
+            piece.toggled = false
+            newList.push(piece)
+        }
+        else {
+            console.log("eliminating", piece.color, piece.type)
+        }
+    }
+    console.log(newList)
+    moving.move(coords)
+    draw()
+    for (let piece of newList) {
+        drawPiece(piece)
+    }
+    return newList
+}
 function handleClick(x, y) {
     if ( x > xOffset && x < (xOffset + 8 * 80)) {
         if (y > yOffset && y < (yOffset + 8 * 80)) {
@@ -89,15 +108,28 @@ function handleClick(x, y) {
 }
 document.getElementsByTagName("body")[0].addEventListener("click", (e) => {
 
-    console.log("cliked at ", e.clientX,e.clientY, xOffset)
     let coords = handleClick(e.clientX, e.clientY)
+    console.log("cliked at ",coords)
+
     for (let piece of pieces) {
         if (piece.coordinates[0] == coords[0] && piece.coordinates[1] == coords[1]) {
-           let moves = piece.moves()
-           for (let c of moves) {
-            highlight(c)
-           }
+            piece.toggled = !piece.toggled
+        } else {
+            if (piece.toggled) {
+                for (let coord of piece.moves(pieces)) {
+                    if ((coord[0] == coords[0] && coord[1] == coords[1]) && turn == piece.color ) {
+                        pieces =  handleMove(pieces, coords, piece)
+                        if (turn == "white") {
+                            turn = "black"
+                        } else {
+                            turn = "white"
+                        }
+                    }
+                }
+            }
         }
     }
+
+
 
 })
--- chess/lib/classes.js
+++ chess/lib/classes.js
@@ -3,8 +3,9 @@ export class Piece {
         this.type = type
         this.color = color
         this.coordinates = coordinates
+        this.toggled = false
     }
-    moves() {
+    moves(pieces) {
         switch(this.type) {
             case "king" :
                 let list = []
@@ -23,6 +24,17 @@ export class Piece {
                 while((a >= 0 && b >= 0) && (a < 8 && b < 8)) {
                     if (!(a == this.coordinates[0] && b == this.coordinates[1])) {
                         squares.push([a,b])
+                        for (let piece of pieces) {
+                            if (a == piece.coordinates[0] && b == piece.coordinates[1]) {
+                                a = -10
+                                if (piece.color == this.color) {
+                                    squares.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
                     }
                     a--;
                     b--;
@@ -32,6 +44,17 @@ export class Piece {
                 while((a >= 0 && b >= 0) && (a < 8 && b < 8)) {
                     if (!(a == this.coordinates[0] && b == this.coordinates[1])) {
                         squares.push([a,b])
+                        for (let piece of pieces) {
+                            if (a == piece.coordinates[0] && b == piece.coordinates[1]) {
+                                a = -10
+                                if (piece.color == this.color) {
+                                    squares.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
                     }
                     a--;
                     b++;
@@ -41,6 +64,17 @@ export class Piece {
                 while((a >= 0 && b >= 0) && (a < 8 && b < 8)) {
                     if (!(a == this.coordinates[0] && b == this.coordinates[1])) {
                         squares.push([a,b])
+                        for (let piece of pieces) {
+                            if (a == piece.coordinates[0] && b == piece.coordinates[1]) {
+                                a = -10
+                                if (piece.color == this.color) {
+                                    squares.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
                     }
                     a++;
                     b--;
@@ -50,7 +84,19 @@ export class Piece {
                 while((a >= 0 && b >= 0) && (a < 8 && b < 8)) {
                     if (!(a == this.coordinates[0] && b == this.coordinates[1])) {
                         squares.push([a,b])
+                        for (let piece of pieces) {
+                            if (a == piece.coordinates[0] && b == piece.coordinates[1]) {
+                                a = -10
+                                if (piece.color == this.color) {
+                                    squares.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
                     }
+
                     a++;
                     b++;
                     
@@ -60,18 +106,86 @@ export class Piece {
                 break;
             case "rook":
                 let moves = []
-                for (let a = 0; a <  8; a++) {
-                    for (let b = 0; b < 8; b++) {
-                        if ((a == this.coordinates[0] || b == this.coordinates[1]) && !(a == this.coordinates[0] && b == this.coordinates[1])){
-                            moves.push([a,b])
-                    
+                let [x, y] = this.coordinates
+                while((x >= 0 && y >= 0) && (x < 8 && y < 8)) {
+                    if (!(x == this.coordinates[0] && y == this.coordinates[1])) {
+                        moves.push([x,y])
+                        for (let piece of pieces) {
+                            if (x == piece.coordinates[0] && y == piece.coordinates[1]) {
+                                x = -10
+                                if (piece.color == this.color) {
+                                    moves.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
                         }
                     }
+
+                    x++;
+                }
+                [x, y] = this.coordinates
+                while((x >= 0 && y >= 0) && (x < 8 && y < 8)) {
+                    if (!(x == this.coordinates[0] && y == this.coordinates[1])) {
+                        moves.push([x,y])
+                        for (let piece of pieces) {
+                            if (x == piece.coordinates[0] && y == piece.coordinates[1]) {
+                                x = -10
+                                if (piece.color == this.color) {
+                                    moves.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
+                    }
+
+                    x--;
+                }
+                [x, y] = this.coordinates
+                while((x >= 0 && y >= 0) && (x < 8 && y < 8)) {
+                    if (!(x == this.coordinates[0] && y == this.coordinates[1])) {
+                        moves.push([x,y])
+                        for (let piece of pieces) {
+                            if (x == piece.coordinates[0] && y == piece.coordinates[1]) {
+                                x = -10
+                                if (piece.color == this.color) {
+                                    moves.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
+                    }
+
+                    y++;
+                }
+                 [x, y] = this.coordinates
+                while((x >= 0 && y >= 0) && (x < 8 && y < 8)) {
+                    if (!(x == this.coordinates[0] && y == this.coordinates[1])) {
+                        moves.push([x,y])
+                        for (let piece of pieces) {
+                            if (x == piece.coordinates[0] && y == piece.coordinates[1]) {
+                                x = -10
+                                if (piece.color == this.color) {
+                                    moves.pop()
+                                }
+                                else {
+                                    console.log(piece.color)
+                                }
+                            }
+                        }
+                    }
+
+                    y--;
                 }
                 return moves
                 break;
             case "queen":
-                return [new Piece("rook", this.color, this.coordinates).moves(), new Piece("bishop", this.color, this.coordinates).moves()].flat()
+                return [new Piece("rook", this.color, this.coordinates).moves(pieces), new Piece("bishop", this.color, this.coordinates).moves(pieces)].flat()
             case "knight":
                 let pre = []
                 let [an,bn] = this.coordinates
@@ -85,7 +199,13 @@ export class Piece {
                 pre.push([an-1, bn-2])
                 let final = []
                 for (let coord of pre) {
-                    if ((coord[0] >= 0 && coord[0] < 8) && (coord[1] >= 0 && coord[1] < 8)) {
+                    let illegal = false
+                    for (let piece of pieces) {
+                        if (piece.color == this.color & (piece.coordinates[0] == coord[0] && piece.coordinates[1] == coord[1])) {
+                            illegal = true
+                        }
+                    }
+                    if ((coord[0] >= 0 && coord[0] < 8) && (coord[1] >= 0 && coord[1] < 8) && !illegal) {
                         final.push(coord)
                     } 
                 }
@@ -95,9 +215,71 @@ export class Piece {
             case "pawn":
                 let pres = []
                 if (this.color == "black") {
-                    pres.push([this.coordinates[0] , this.coordinates[1] +1 ])
+                    let ouccupied = false
+                    let ouccupiedRight = false
+                    let ouccupiedLeft = false 
+                    let second = false
+                    for (let piece of pieces) {
+                        if (((piece.coordinates[0] == this.coordinates[0] && piece.coordinates[1] == this.coordinates[1] + 1))) {
+                            ouccupied = true
+                        }
+                        if (((piece.coordinates[0] == this.coordinates[0] && piece.coordinates[1] == this.coordinates[1] + 2))) {
+                            second = true
+                        }
+                        if (((piece.coordinates[0] == this.coordinates[0] +1 && piece.coordinates[1] == this.coordinates[1] + 1)) && piece.color == "white") {
+                            ouccupiedRight = true
+                        }
+                        if (((piece.coordinates[0] == this.coordinates[0] -1 && piece.coordinates[1] == this.coordinates[1] + 1)) && piece.color == "white") {
+                            ouccupiedLeft = true
+                        }
+                    }
+                    if (!ouccupied) {
+                        pres.push([this.coordinates[0] , this.coordinates[1] +1 ])
+                        console.log("forward",[this.coordinates[0] , this.coordinates[1] +1 ] )
+                    }
+                    if (ouccupiedRight) {
+                        pres.push([this.coordinates[0] + 1, this.coordinates[1] + 1])
+                        console.log("right", [this.coordinates[0] - 1, this.coordinates[1] + 1])
+                    }
+                    if (ouccupiedLeft) {
+                        pres.push([this.coordinates[0] - 1, this.coordinates[1] + 1])
+                        console.log("left", [this.coordinates[0] - 1, this.coordinates[1] + 1])
+                    }
+                    if (this.coordinates[1] == 1 && !second) {
+                       pres.push([this.coordinates[0],3 ]) 
+                    }
+
                 } if (this.color == "white") {
-                    pres.push([this.coordinates[0]  , this.coordinates[1] -1 ])                  
+                    let ouccupied = false
+                    let ouccupiedRight = false
+                    let ouccupiedLeft = false 
+                    let second
+                    for (let piece of pieces) {
+                        if (((piece.coordinates[0] == this.coordinates[0] && piece.coordinates[1] == this.coordinates[1] - 1))) {
+                            ouccupied = true
+                        }
+                        if (((piece.coordinates[0] == this.coordinates[0] && piece.coordinates[1] == this.coordinates[1] -2 ))) {
+                            second = true
+                        }
+                        if (((piece.coordinates[0] == this.coordinates[0] +1 && piece.coordinates[1] == this.coordinates[1] - 1)) && piece.color == "black") {
+                            ouccupiedRight = true
+                        }
+                        if (((piece.coordinates[0] == this.coordinates[0] -1 && piece.coordinates[1] == this.coordinates[1] - 1)) && piece.color == "black") {
+                            ouccupiedLeft = true
+                        }
+                    }
+                    if (!ouccupied) {
+                        pres.push([this.coordinates[0] , this.coordinates[1] -1 ])
+                    }
+                    if (ouccupiedRight) {
+                        pres.push([this.coordinates[0] + 1, this.coordinates[1] - 1])
+                    }
+                    if (ouccupiedLeft) {
+                        pres.push([this.coordinates[0] - 1, this.coordinates[1] - 1])
+                    }
+                    if ( this.coordinates[1] == 6 && !second) {
+                        pres.push([this.coordinates[0], 4])
+                    }
                 }
                 let finals = []
                 for (let coord of pres) {
@@ -110,5 +292,9 @@ export class Piece {
             default : 
                 break;
         }
+    }
+    move(coords) {
+        this.coordinates = coords
+        console.log("moved")
     }
 } 
